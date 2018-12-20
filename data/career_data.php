3522e53442b0b5ab86ac885d7f6bad3902f3b162
YanaBakinovskaya <yananik3@gmail.com>
2018-12-21 00:09:21 +0200

    typo;

--- data/career_data.php
+++ data/career_data.php
@@ -19,7 +19,7 @@
       [
         'title' => 'Место работы №1',
         'post' => 'Должность №1',
-        'city' => 'Днепр',
+        'city' => 'Днипро',
         'company' => 'Компания №1',
         'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has bees standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book . It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
         'yearStart' => '2007',
@@ -37,7 +37,7 @@
       [
         'title' => 'Место работы №3',
         'post' => 'Должность №3',
-        'city' => 'Днепр',
+        'city' => 'Днипро',
         'company' => 'Компания №3',
         'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has bees standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book . It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
         'yearStart' => '2013',
